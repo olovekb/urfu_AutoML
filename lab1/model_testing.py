e7b050a01f736195b657ad899824ed5ab8900cc8
Shoolife <ivan14.02.2003@gmail.com>
2025-02-22 21:34:35 +0500

    Update

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 import pandas as pd
+import numpy as np
 import pickle
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Загружаем масштабированные данные для теста
-test_file = "test/test_data_scaled.csv"
-df_test = pd.read_csv(test_file)
-X_test = df_test[['day', 'temp']]
-y_test = df_test['label']
+df_test = pd.read_csv("test/test_data_scaled.csv")
 
-# Загружаем сохраненную модель
+# Формируем те же признаки
+X_test = pd.DataFrame()
+X_test['sin_day'] = np.sin(2 * np.pi * df_test['day'] / 30)
+X_test['cos_day'] = np.cos(2 * np.pi * df_test['day'] / 30)
+X_test['humidity'] = df_test['humidity']
+
+y_test = df_test['temp']
+
+# Загружаем сохранённую модель
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-# Выполняем предсказание и вычисляем accuracy
+# Выполняем предсказание и оцениваем качество
 y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-print(f"Model test accuracy is: {acc:.3f}")
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse:.3f}")
+print(f"R^2 Score: {r2:.3f}")
